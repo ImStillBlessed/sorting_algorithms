89541620a652ca29c3e405dbfc3fb83a5b07ab35
Oigbochie Blessed <eprince1800@gmail.com>
2023-11-16 12:57:49 +0100

    transfer to phone cause of  GCC error

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,39 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *ptr;
-	
-	ptr = head
-	while(ptr
+	listint_t *current = *list;
+	listint_t *next;
+	listint_t *sorted = NULL;
+	listint_t *temp;
+
+	while(current != NULL)
+	{
+		next = current->next;
+		if(sorted == NULL || sorted->n >= current->n)
+		{
+			current->next = sorted;
+			current->prev = NULL;
+			if (sorted != NULL)
+				sorted->prev = current;
+			sorted = current;
+		}
+		else
+		{
+			temp = sorted;
+			while (temp->next != NULL && temp->next->n < current->n)
+			{
+				temp = temp->next;
+			}
+			current->next = temp->next;
+			if (temp->next != NULL)
+			{
+				temp->next->prev = current;
+			}
+			temp->next = current;
+			current->prev = temp;
+			print_list(sorted);
+		}
+		current = next;
+	}
+	*list = sorted;
+}
