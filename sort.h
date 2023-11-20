73264c20f5685222ac8a96eb677b88b9a5129859
Blessed Oigbochie <eprince1800@gmail.com>
2023-11-20 13:50:48 +0100

    quick sort

--- sort.h
+++ sort.h
@@ -36,6 +36,7 @@ int *swap_array(int *array, size_t position);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 #endif /* SORT_H */
